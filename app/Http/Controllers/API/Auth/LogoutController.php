0eaa11bed9fbff1ebbd20fc0a392531e588053eb
Kezia Regina <keziaregina28@gmail.com>
2024-12-28 15:08:29 +0700

    fix: users auth endpoints

--- app/Http/Controllers/API/Auth/LogoutController.php
+++ app/Http/Controllers/API/Auth/LogoutController.php
@@ -1,15 +1,12 @@
 <?php
 
-namespace App\Http\Controllers\API\halalstyle\Auth\V2;
+namespace App\Http\Controllers\API\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class LogoutController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     */
     public function __invoke(Request $request)
     {
         $request->user()->currentAccessToken()->delete();
