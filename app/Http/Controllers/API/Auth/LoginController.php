e100c60e16bfb8289447e9d9ceeba719bea9e1c3
Dimas Ahmad Rizal <mail.dimasrizal@gmail.com>
2025-01-07 11:54:29 +0700

    fix error login with nik or phone

--- app/Http/Controllers/API/Auth/LoginController.php
+++ app/Http/Controllers/API/Auth/LoginController.php
@@ -4,41 +4,27 @@ namespace App\Http\Controllers\API\Auth;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Resources\ResponseJsonResource;
 
 class LoginController extends Controller
 {
-    public function __invoke(Request $request) {
-        $request->validate([
-            'nik' => 'required|string',
-            'password' => 'required|string',
+    public function __invoke(Request $request)
+    {
+        $credentials = $request->validate([
+            'nik' => ['required'],
+            'password' => ['required'],
         ]);
-        
-        $credentials = [
-            'password' => $request->password,
-        ];
 
-        if (preg_match('/^\d{16}$/', $request->nik)) {
-            $credentials['nik'] = $request->nik;
-        } else {
-            $credentials['phone'] = $request->nik;
-        }
-
-        if (!auth()->attempt($credentials,true)) {
-            return response()->json([
-                'message' => 'Invalid login details'
-            ], 401);
-        }
-
-    
-        $user = auth()->user();
-        // $user->update(['last_online' => now()]);
-    
-        $token         = $user->createToken('mobile')->plainTextToken;
-        $data['user']  = $user->load(['roles', 'province', 'city', 'district', 'village']);
-        $data['token'] = $token;
-    
-        return new ResponseJsonResource($data, 'Login success');
+            if (Auth::attempt(['nik' => $credentials['nik'], 'password' => $credentials['password']]) || Auth::attempt(['phone' => $credentials['nik'], 'password' => $credentials['password']])) {
+                $user = auth()->user();
+            
+                $token         = $user->createToken('mobile')->plainTextToken;
+                $data['user']  = $user->load(['roles', 'province', 'city', 'district', 'village']);
+                $data['token'] = $token;
+            
+                return new ResponseJsonResource($data, 'Login success');
+            }
+            return new ResponseJsonResource(null, 'The provided credentials do not match our records.', 401);
     }
-
 }
